47bc8c3f5162fab29b2034b4ec8dfe8efd105a9a
Vijit Shrivastava <shrivastavavijit@gmail.com>
2021-10-07 02:19:44 +0530

    Donut chart graph, Added header-drawer for small screens

--- src/components/DashboardContainer/index.jsx
+++ src/components/DashboardContainer/index.jsx
@@ -5,8 +5,8 @@ import MyChartContainer from "../MyChartContainer";
 import ChartBox from "../ChartBox";
 import LineChart from "../Visualisations/LineChart";
 import ColumnBarGraph from "../Visualisations/ColumnBarGraph";
-import RadialChart from "../Visualisations/RadialBar";
 import CountChart from "../Visualisations/CountChart";
+import DonutChart from "../Visualisations/DonutChart";
 
 const CustomButton = styled(Button)`
   padding: 8px 32px;
@@ -39,7 +39,6 @@ const Banner = styled(Box)`
   background-color: #075be0;
   color: rgba(0, 0, 0, 0.87);
   border-radius: 4px;
-  height: 190px;
   display: flex;
   align-items: center;
 
@@ -60,11 +59,8 @@ const Banner = styled(Box)`
   }
 
   .banner-text {
-    /* align-items: center;
-    justify-content: center; */
     display: flex;
     flex-direction: column;
-    /* width: 100%; */
     margin: 5%;
     .header {
       color: white;
@@ -77,13 +73,6 @@ const Banner = styled(Box)`
 `;
 
 function DashboardContainer() {
-  const expandToFullScreen = (chartType) => {
-    const elem = document.getElementById(`chart-container-${chartType}`);
-    if (elem?.requestFullscreen) elem.requestFullscreen();
-    else if (elem?.webkitRequestFullscreen) elem.webkitRequestFullscreen();
-    else if (elem?.msRequestFullscreen) elem.msRequestFullscreen();
-  };
-
   return (
     <Container fluid style={{ marginTop: "16px" }}>
       <Grid container spacing={2}>
@@ -117,7 +106,6 @@ function DashboardContainer() {
         <Grid xs={12} sm={12} md={4}>
           <MyChartContainer
             text={"Monthly Stats"}
-            // content={<ChartBox chart={LineChart} />}
             content={<ChartBox chart={CountChart} />}
           />
         </Grid>
@@ -128,14 +116,7 @@ function DashboardContainer() {
               <div id="chart-container-column">
                 <ChartBox
                   header="Sales"
-                  action={
-                    <Button
-                      variant="text"
-                      onClick={() => expandToFullScreen("column")}
-                    >
-                      Expand
-                    </Button>
-                  }
+                  action={<Button variant="text">View</Button>}
                   chart={ColumnBarGraph}
                 />
               </div>
@@ -150,46 +131,23 @@ function DashboardContainer() {
               content={
                 <ChartBox
                   header="Clicks Trend"
-                  action={
-                    <Button
-                      variant="text"
-                      onClick={() => expandToFullScreen("line")}
-                    >
-                      Expand
-                    </Button>
-                  }
+                  action={<Button variant="text">View</Button>}
                   chart={LineChart}
                 />
               }
             />
           </div>
         </Grid>
-
-        {/* <Grid xs={12} sm={6} md={4}>
-          <div id="chart-container-line">
+        <Grid xs={12} sm={6} md={4}>
+          <div id="chart-container-donut">
             <MyChartContainer
               text={"Trends"}
               content={
                 <ChartBox
-                  header="Clicks Trend"
-                  action={<Button variant="primary">New Task</Button>}
-                  chart={
-                    <>
-                      <Banner />
-                    </>
-                  }
+                  header="Views"
+                  action={<Button variant="text">View</Button>}
+                  chart={<div>{DonutChart}</div>}
                 />
-              }
-            />
-          </div>
-        </Grid> */}
-
-        <Grid xs={12} sm={6} md={4}>
-          <div id="chart-container-radial">
-            <MyChartContainer
-              text={"User Visits"}
-              content={
-                <ChartBox header="Views" chart={<div>{RadialChart}</div>} />
               }
             />
           </div>
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,40 @@
-import React, { useState } from "react";
-// import { styled } from "@mui/material/styles";
+import React from "react";
+import { useTheme } from "@mui/material/styles";
+
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Badge from "@mui/material/Badge";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
+import List from "@mui/material/List";
+
+import Drawer from "@mui/material/Drawer";
+import Divider from "@mui/material/Divider";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+
 import Avatar from "@mui/material/Avatar";
 import BellIcon from "@mui/icons-material/Notifications";
-import { BottomNavigation, BottomNavigationAction } from "@mui/material";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import MoreIcon from "@mui/icons-material/MoreVert";
+
+import {
+  BottomNavigation,
+  BottomNavigationAction,
+  Menu,
+  MenuItem,
+} from "@mui/material";
 import NavigationData from "./constants";
 import styled from "styled-components";
+
+const drawerWidth = 240;
 
 const StyledBottomNavigationAction = styled(BottomNavigationAction)`
   flex-direction: row;
@@ -30,7 +54,114 @@ const StyledAvatar = styled(Avatar)`
 `;
 
 export default function Header() {
-  const [navData, setnavData] = useState(NavigationData);
+  // const [navData, setnavData] = useState(NavigationData)
+  const theme = useTheme();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const mobileMenuId = "primary-search-account-menu";
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id={"mobileMenuId"}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton
+          size="large"
+          aria-label="show 17 new notifications"
+          color="inherit"
+        >
+          <Badge badgeContent={17} color="error">
+            <BellIcon />
+          </Badge>
+        </IconButton>
+        <p>Notifications</p>
+      </MenuItem>
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
+
+  const drawerNav = (
+    <Drawer
+      sx={{
+        width: drawerWidth,
+        flexShrink: 0,
+        "& .MuiDrawer-paper": {
+          width: drawerWidth,
+        },
+      }}
+      variant="persistent"
+      anchor="right"
+      open={open}
+    >
+      <div>
+        <IconButton onClick={handleDrawerClose}>
+          {theme.direction === "rtl" ? (
+            <ChevronLeftIcon />
+          ) : (
+            <ChevronRightIcon />
+          )}
+        </IconButton>
+      </div>
+      <Divider />
+      <List>
+        {NavigationData.map((x, index) => (
+          <ListItem button key={x.label}>
+            <ListItemIcon>{x.icon}</ListItemIcon>
+            <ListItemText primary={x.label} />
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
+  );
+
   return (
     <AppBar position="sticky" color="inherit">
       <Toolbar>
@@ -57,17 +188,36 @@ export default function Header() {
             <StyledAvatar>V</StyledAvatar>
           </Box>
         </Box>
+        <Box sx={{ display: { xs: "flex", md: "none" } }}>
+          <IconButton
+            size="large"
+            aria-label="show more"
+            aria-controls={mobileMenuId}
+            aria-haspopup="true"
+            onClick={handleMobileMenuOpen}
+            color="inherit"
+          >
+            <MoreIcon />
+          </IconButton>
+        </Box>
+        <Box sx={{ display: { xs: "flex", md: "none" } }}>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="end"
+            onClick={handleDrawerOpen}
+            sx={{ ...(open && { display: "none" }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Box>
       </Toolbar>
 
       <BottomNavigation
         sx={{ display: { xs: "none", md: "flex" } }}
         showLabels
         value={"/"}
-        onChange={(event, newValue) => {
-          // console.log(newValue);
-          // const newNavdata = navData[newValue] && navData[newValue].active = true || [];
-          // setnavData(newNavdata);
-        }}
+        onChange={(event, newValue) => {}}
       >
         {NavigationData.map((el, i) => (
           <StyledBottomNavigationAction
@@ -78,6 +228,8 @@ export default function Header() {
           />
         ))}
       </BottomNavigation>
+      {renderMobileMenu}
+      {drawerNav}
     </AppBar>
   );
 }
